3a5de510adb7f0bb6e27c881e9e58049eee3a99e
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:14:21 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan4/kambing/PBO11K10118907Latihan4Kambing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan4.kambing;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan membuat fungsi
+ * 
+ */
+public class PBO11K10118907Latihan4Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void tambahKambing() {
+        int jumlahKambing = 0;
+
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: "+jumlahKambing);
+    }
+
+    public static void main(String[] args) {
+        PBO11K10118907Latihan4Kambing kambingJantan = new PBO11K10118907Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
